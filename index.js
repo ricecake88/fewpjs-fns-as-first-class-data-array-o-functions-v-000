67f679b782966414e35d9ed7ed339ae41d7d9db7
Grace Shih <vocalise@gmail.com>
2019-12-19 05:54:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ function unleashDog(dogName, dogBreed) {
 
 function exerciseDog(dogName, dogBreed) {
   let routine = ["wakeDog", "leashDog", "walkToPark", "throwFrisbee", "walkHome", "unleashDog"];
-  let result = [];
-  result.push(routine.forEach(;
-  return push;
+for (var i = 0; i < arrayOfFunctions.length; ++i) {
+    arrayOfFunctions[i](); // run your function
+}
 }
 
