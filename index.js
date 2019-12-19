298fab35a52733132c03dd483663781a1697da87
Grace Shih <vocalise@gmail.com>
2019-12-19 05:50:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 function wakeDog(dogName, dogBreed) {
-  console.log("Wake ${dogName} the ${dogBreed}")
+  str = "Wake ${dogName} the ${dogBreed}";
+  console.log();
 }
 
 function leashDog(dogName, dogBreed) {
-  console.log("Leash ${dogName} the ${dogBreed}")  
+  str = "Leash ${dogName} the ${dogBreed}";
 }
 
 function walkToPark(dogName, dogBreed) {
-  console.log("Walk to the park with ${dogName} the ${dogBreed}")  
+  str = "Walk to the park with ${dogName} the ${dogBreed}";  
 }
 
 function throwFrisbee(dogName, dogBreed) {
-  console.log("Throw the frisbee for ${dogName} the ${dogBreed}")  
+  str = "Throw the frisbee for ${dogName} the ${dogBreed}"; 
 }
 
 function walkHome(dogName, dogBreed) {
-  console.log("Walk home with ${dogName} the ${dogBreed}")  
+  str = "Walk home with ${dogName} the ${dogBreed}";  
 }
 
 function unleashDog(dogName, dogBreed) {
-  console.log("Unleash ${dogName} the ${dogBreed}")  
+  str = "Unleash ${dogName} the ${dogBreed}";  
 }
 
 function exerciseDog(dogName, dogBreed) {
