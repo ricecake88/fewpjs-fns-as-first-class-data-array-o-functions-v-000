c6f3e365339e5ec99b0bf748adcc212d5e0c768f
Grace Shih <vocalise@gmail.com>
2019-12-19 05:39:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function wakeDog(dogName, dogBreed) {
-  
+  console.log("Wake ${dogName} the ${dogBreed}")
 }
 
 function leashDog(dogName, dogBreed) {
-  
+  console.log("Wake ${dogName} the ${dogBreed}")  
 }
 
 function walkToPark(dogName, dogBreed) {
